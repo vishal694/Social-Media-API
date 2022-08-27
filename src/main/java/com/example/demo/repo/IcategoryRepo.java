9f4ae0be3fbfd079350a9f4a400de804e03a9304
vishal <kishanmodi13187@gmail.com>
2022-08-27 10:51:20 +0530

    change names

--- src/main/java/com/example/demo/repo/IcategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.bean.Category;
-
-public interface ICategoryRepo extends JpaRepository<Category, Integer> {
-
-}
--- src/main/java/com/example/demo/repo/IcommentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.bean.Comment;
-
-public interface ICommentRepo extends JpaRepository<Comment, Integer> {
-
-}
